a2a742c61c463a90ceec6a059b4d5e035ea7e497
MightBeFonias <fatihnaufal48@gmail.com>
2026-09-17 02:07:34 +0700

    adding JSON endpoint for Awards

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_awards, show_main, show_experience, create_experience, create_award, get_experiences_json, delete_experience, delete_award
+from main.views import show_awards, show_main, show_experience, create_experience, create_award, get_experiences_json, get_awards_json, delete_experience, delete_award
 
 app_name = "main"
 
@@ -9,8 +9,9 @@ urlpatterns = [
     path("experience/", show_experience, name="show_experience"),
     path("awards/", show_awards, name="show_awards"),
     path("experience/add/", create_experience, name="create_experience"),
-    path("api/projects/", get_experiences_json, name="get_experiences_json"),
+    path("api/experiences/", get_experiences_json, name="get_experiences_json"),
     path("awards/add/", create_award, name="create_award"),
+    path("api/awards/", get_awards_json, name="get_awards_json"),
     path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
     path("awards/<uuid:award_id>/delete/", delete_award, name="delete_award")
 ]
